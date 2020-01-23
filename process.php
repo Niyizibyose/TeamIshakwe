79979b4df64857bf6f65b53bd65ddb73bbd4c840
Niyizibyose <jean21101999@gmail.com>
2020-01-23 19:52:20 +0200

    login-php

--- process.php
+++ process.php
@@ -1,11 +1,18 @@
 <?php
 
-$username = $_POST ['username'];
+$username = $_POST ['username'];       //values from login.php
 $password = $_POST['password'];
 
 $username = stripslashes ($username);
-$password = stripslashes ($password);
+$password = stripslashes ($password);               //mysql injection prevention
 $username = mysql_real_escape_string ($username);
 $password = mysql_real_escape_string ($password);
 
+
+mysql_connect ("localhost" , "root" , "");
+mysql_select_db ("foot-sers");                  //server and database connection
+
+$result = mysql_query ("select from users where username = '$username' and password = '$password'")
+ or die("Failed to query database " .mysql_error());
 ?>
+
