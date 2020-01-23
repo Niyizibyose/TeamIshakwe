43ece4e5c0a5e0e8451996d9aa3fbd61273ee0e6
Niyizibyose <jean21101999@gmail.com>
2020-01-23 20:00:38 +0200

    post-function-added

--- login.php
+++ login.php
@@ -42,10 +42,10 @@
 
                   <form class="user" action="process.php">
                     <div class="form-group">
-                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..." name="username">
                     </div>
                     <div class="form-group">
-                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                      <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password" name="password">
                     </div>
                     <div class="form-group">
                       <div class="custom-control custom-checkbox small">
--- process.php
+++ process.php
@@ -14,5 +14,13 @@ mysql_select_db ("foot-sers");                  //server and database connection
 
 $result = mysql_query ("select from users where username = '$username' and password = '$password'")
  or die("Failed to query database " .mysql_error());
+
+ $row = mysql_fetch_array ($result);
+
+ if ($row ['username'] == $username && $row ['password'] == $password){
+     echo "Login Success ! Welcome ".$row['username'];
+ } else {
+     echo "Failed to login!";
+ }
 ?>
 
