03abfa8c246a01204479c27e3a61a9ec1e562f9e
Niyizibyose <jean21101999@gmail.com>
2020-01-27 19:54:21 +0200

    statistics-page

--- charts.php
+++ charts.php
@@ -373,9 +373,8 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-2 text-gray-800">Charts</h1>
-          <p class="mb-4">Chart.js is a third party plugin that is used to generate the charts in this theme. The charts below have been customized - for further customization options, please visit the <a target="_blank" href="https://www.chartjs.org/docs/latest/">official Chart.js documentation</a>.</p>
-
+          <h1 class="h3 mb-2 text-gray-800"> Statistics</h1>
+         
           <!-- Content Row -->
           <div class="row">
 
@@ -384,7 +383,7 @@
               <!-- Area Chart -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Area Chart</h6>
+                  <h6 class="m-0 font-weight-bold text-primary"> Amafaranga</h6>
                 </div>
                 <div class="card-body">
                   <div class="chart-area">
@@ -398,7 +397,7 @@
               <!-- Bar Chart -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Bar Chart</h6>
+                  <h6 class="m-0 font-weight-bold text-primary"> Ibitego</h6>
                 </div>
                 <div class="card-body">
                   <div class="chart-bar">
@@ -416,7 +415,7 @@
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Donut Chart</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">Urwunguko</h6>
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
