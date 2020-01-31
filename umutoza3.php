8b301c4b8a99669a929967dd7115e3392a2db72b
Niyizibyose <jean21101999@gmail.com>
2020-01-31 20:18:15 +0200

    abatoza-pages

--- umutoza3.php
+++ umutoza3.php
@@ -372,7 +372,7 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Umutoza w'abazamu</h1>
+          <h1 class="h3 mb-4 text-gray-800">Umutoza mukuru</h1>
 
           <div class="row">
 
@@ -381,32 +381,44 @@
               <!-- Circle Buttons -->
               <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">Raporo y'ukwezi</h6>
+                <h6 class=""> <a href="https://docs.google.com/document/d/1zWy1aEDmw1m1GkCn_xVCdTsNu9qYnnEfs1WYH-1q7R0/edit" target="_blank">Raporo y'ukwezi kwa 9/2019 </a> </h6>
+                </div>
+
+                <div class="card-header py-3">
+                <h6 class=""> <a href="https://docs.google.com/document/d/1zWy1aEDmw1m1GkCn_xVCdTsNu9qYnnEfs1WYH-1q7R0/edit" target="_blank">Raporo y'ukwezi kwa 8/2019 </a> </h6>
+                </div>
+
+                <div class="card-header py-3">
+                <h6 class=""> <a href="https://docs.google.com/document/d/1zWy1aEDmw1m1GkCn_xVCdTsNu9qYnnEfs1WYH-1q7R0/edit" target="_blank">Raporo y'ukwezi kwa 7/2019 </a> </h6>
+                </div>
+
+                <div class="card-header py-3">
+                <h6 class=""> <a href="https://docs.google.com/document/d/1zWy1aEDmw1m1GkCn_xVCdTsNu9qYnnEfs1WYH-1q7R0/edit" target="_blank">Raporo y'ukwezi kwa 6/2019 </a> </h6>
+                </div>
+
+                <div class="card-header py-3">
+                <h6 class=""> <a href="https://docs.google.com/document/d/1zWy1aEDmw1m1GkCn_xVCdTsNu9qYnnEfs1WYH-1q7R0/edit" target="_blank">Raporo y'ukwezi kwa 5/2019 </a> </h6>
+                </div>
+
+                <div class="card-header py-3">
+                <h6 class=""> <a href="https://docs.google.com/document/d/1zWy1aEDmw1m1GkCn_xVCdTsNu9qYnnEfs1WYH-1q7R0/edit" target="_blank">Raporo y'ukwezi kwa 4/2019 </a> </h6>
+                </div>
+
+                <div class="card-header py-3">
+                  <h6 class=""> <a href="https://docs.google.com/document/d/1zWy1aEDmw1m1GkCn_xVCdTsNu9qYnnEfs1WYH-1q7R0/edit" target="_blank">Raporo y'ukwezi kwa 3/2019 </a> </h6>
+                </div>
+
+                <div class="card-header py-3">
+                <h6 class=""> <a href="https://docs.google.com/document/d/1zWy1aEDmw1m1GkCn_xVCdTsNu9qYnnEfs1WYH-1q7R0/edit" target="_blank">Raporo y'ukwezi kwa 2/2019 </a> </h6>
+                
                 </div>
                 <div class="card-body">
 
                   <!-- Circle Buttons (Default) -->
-                  <div class="mb-2">
-                    <code>.btn-circle</code>
-                  </div>
-                  <a href="#" class="btn btn-primary btn-circle">
-                    <i class="fab fa-facebook-f"></i>
-                  </a>
-                  <a href="#" class="btn btn-success btn-circle">
-                    <i class="fas fa-check"></i>
-                  </a>
-                  <a href="#" class="btn btn-info btn-circle">
-                    <i class="fas fa-info-circle"></i>
-                  </a>
-                  <a href="#" class="btn btn-warning btn-circle">
-                    <i class="fas fa-exclamation-triangle"></i>
-                  </a>
-                  <a href="#" class="btn btn-danger btn-circle">
-                    <i class="fas fa-trash"></i>
-                  </a>
+          
                   <!-- Circle Buttons (Small) -->
                   <div class="mt-4 mb-2">
-                    <code>.btn-circle .btn-sm</code>
+                    
                   </div>
                   <a href="#" class="btn btn-primary btn-circle btn-sm">
                     <i class="fab fa-facebook-f"></i>
@@ -423,27 +435,8 @@
                   <a href="#" class="btn btn-danger btn-circle btn-sm">
                     <i class="fas fa-trash"></i>
                   </a>
-                  <!-- Circle Buttons (Large) -->
-                  <div class="mt-4 mb-2">
-                    <code>.btn-circle .btn-lg</code>
-                  </div>
-                  <a href="#" class="btn btn-primary btn-circle btn-lg">
-                    <i class="fab fa-facebook-f"></i>
-                  </a>
-                  <a href="#" class="btn btn-success btn-circle btn-lg">
-                    <i class="fas fa-check"></i>
-                  </a>
-                  <a href="#" class="btn btn-info btn-circle btn-lg">
-                    <i class="fas fa-info-circle"></i>
-                  </a>
-                  <a href="#" class="btn btn-warning btn-circle btn-lg">
-                    <i class="fas fa-exclamation-triangle"></i>
-                  </a>
-                  <a href="#" class="btn btn-danger btn-circle btn-lg">
-                    <i class="fas fa-trash"></i>
-                  </a>
-                </div>
-              </div>
+
+              </div> 
 
               <!-- Brand Buttons -->
               <div class="card shadow mb-4">
@@ -451,10 +444,15 @@
                   <h6 class="m-0 font-weight-bold text-primary">Ubuzima bw'abakinnyi</h6>
                 </div>
                 <div class="card-body">
-                  <p>Google and Facebook buttons are available featuring each company's respective brand color. They are used on the user login and registration pages.</p>
-                  <p>You can create more custom buttons by adding a new color variable in the <code>_variables.scss</code> file and then using the Bootstrap button variant mixin to create a new style, as demonstrated in the <code>_buttons.scss</code> file.</p>
-                  <a href="#" class="btn btn-google btn-block"><i class="fab fa-google fa-fw"></i> .btn-google</a>
-                  <a href="#" class="btn btn-facebook btn-block"><i class="fab fa-facebook-f fa-fw"></i> .btn-facebook</a>
+                 
+                <div class="my-2"></div>
+                  <a href="abakozi.php" class="btn btn-warning btn-icon-split" target="_blank">
+                    <span class="icon text-white-50">
+                      
+                    </span>
+                    <span class="text">Reba</span>
+                  </a>
+                  <div class="my-2"></div>
 
                 </div>
               </div>
@@ -467,71 +465,17 @@
                 <div class="card-header py-3">
                   <h6 class="m-0 font-weight-bold text-primary">Imishahara</h6>
                 </div>
-                <div class="card-body">
-                  <p>Works with any button colors, just use the <code>.btn-icon-split</code> class and the markup in the examples below. The examples below also use the <code>.text-white-50</code> helper class on the icons for additional styling, but it is not required.</p>
-                  <a href="#" class="btn btn-primary btn-icon-split">
+               
+                  <div class="my-2"></div>
+                  <a href="abakozi.php" class="btn btn-warning btn-icon-split" target="_blank">
                     <span class="icon text-white-50">
-                      <i class="fas fa-flag"></i>
+                      
                     </span>
-                    <span class="text">Split Button Primary</span>
+                    <span class="text">Reba</span>
                   </a>
                   <div class="my-2"></div>
-                  <a href="#" class="btn btn-success btn-icon-split">
-                    <span class="icon text-white-50">
-                      <i class="fas fa-check"></i>
-                    </span>
-                    <span class="text">Split Button Success</span>
-                  </a>
-                  <div class="my-2"></div>
-                  <a href="#" class="btn btn-info btn-icon-split">
-                    <span class="icon text-white-50">
-                      <i class="fas fa-info-circle"></i>
-                    </span>
-                    <span class="text">Split Button Info</span>
-                  </a>
-                  <div class="my-2"></div>
-                  <a href="#" class="btn btn-warning btn-icon-split">
-                    <span class="icon text-white-50">
-                      <i class="fas fa-exclamation-triangle"></i>
-                    </span>
-                    <span class="text">Split Button Warning</span>
-                  </a>
-                  <div class="my-2"></div>
-                  <a href="#" class="btn btn-danger btn-icon-split">
-                    <span class="icon text-white-50">
-                      <i class="fas fa-trash"></i>
-                    </span>
-                    <span class="text">Split Button Danger</span>
-                  </a>
-                  <div class="my-2"></div>
-                  <a href="#" class="btn btn-secondary btn-icon-split">
-                    <span class="icon text-white-50">
-                      <i class="fas fa-arrow-right"></i>
-                    </span>
-                    <span class="text">Split Button Secondary</span>
-                  </a>
-                  <div class="my-2"></div>
-                  <a href="#" class="btn btn-light btn-icon-split">
-                    <span class="icon text-gray-600">
-                      <i class="fas fa-arrow-right"></i>
-                    </span>
-                    <span class="text">Split Button Primary</span>
-                  </a>
-                  <div class="mb-4"></div>
-                  <p>Also works with small and large button classes!</p>
-                  <a href="#" class="btn btn-primary btn-icon-split btn-sm">
-                    <span class="icon text-white-50">
-                      <i class="fas fa-flag"></i>
-                    </span>
-                    <span class="text">Split Button Small</span>
-                  </a>
-                  <div class="my-2"></div>
-                  <a href="#" class="btn btn-primary btn-icon-split btn-lg">
-                    <span class="icon text-white-50">
-                      <i class="fas fa-flag"></i>
-                    </span>
-                    <span class="text">Split Button Large</span>
-                  </a>
+          
+       
                 </div>
               </div>
 
